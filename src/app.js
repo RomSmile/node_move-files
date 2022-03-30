75feae7e378c4c2d05efa1a3b9e1b38a789704b3
Roman Yacenko <ryacenko3@gmail.com>
2022-03-31 01:47:39 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,50 @@
+/* eslint-disable no-console */
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const fs = require('fs');
 
-module.exports = sum;
+function write() {
+  const arr = process.argv.slice(2);
+
+  if (!fs.existsSync(`./src/${arr[0]}`)) {
+    console.log('Your first file does not exist in src');
+
+    return;
+  }
+
+  if (!arr[1].includes('/') && !fs.existsSync(arr[1])) {
+    fs.rename(`./src/${arr[0]}`, `./src/${arr[1]}`, (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+
+    return;
+  }
+
+  if (!fs.existsSync(arr[1])) {
+    console.log('Your first file does not exist in main directory');
+
+    return;
+  }
+
+  fs.readFile(`./src/${arr[0]}`, (err, data) => {
+    if (err) {
+      console.log(err);
+
+      return;
+    }
+
+    fs.writeFile(`${arr[1]}/${arr[0]}`, data, (error) => {
+      if (error) {
+        console.log(err);
+
+        return;
+      }
+
+      fs.unlinkSync(`./src/${arr[0]}`);
+    });
+  });
+};
+
+write();
